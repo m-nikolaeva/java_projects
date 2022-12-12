cff6c7581cd628272a9cdbb36e8a335e51605d69
Marina <mv.nikolaeva1507@gmail.com>
2022-12-12 17:12:46 +0300

    add lecture_3

---  lectures/Lesson_03/Ex000.java
+++  lectures/Lesson_03/Ex000.java
@@ -1,6 +1,18 @@
 package Lesson_03;
+
 public class Ex000 {
    public static void main(String[] args) {
-       
+       System.out.println(Sum(1, 2));
+       System.out.println(Sum(1.0, 2));
+       System.out.println(Sum(1, 2.0));
+       System.out.println(Sum(1.2, 2.1));
+   }
+   static Object Sum(Object a, Object b) {
+
+       if (a instanceof Double && b instanceof Double) {
+           return  (Object)((Double) a + (Double) b);
+       } else if(a instanceof Integer && b instanceof Integer) {
+           return (Object)((Integer) a + (Integer) b);
+       } else return 0;
    }
 }
---  lectures/Lesson_03/Ex005_ArraysMethod.java
+++  lectures/Lesson_03/Ex005_ArraysMethod.java
@@ -1,1 +1,15 @@
 package Lesson_03;
+import java.util.Arrays;
+import java.util.List;
+public class Ex005_ArraysMethod {
+   public static void main(String[] args) {
+       StringBuilder day = new StringBuilder("28");
+       StringBuilder month = new StringBuilder("9");
+       StringBuilder year = new StringBuilder("1990");
+       StringBuilder[] date = { day, month, year };
+       List<StringBuilder> d = Arrays.asList(date);
+       System.out.println(d); // [29, 9, 1990]
+        
+        
+   }
+}
