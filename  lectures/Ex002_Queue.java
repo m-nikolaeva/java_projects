c52a58d9fa37d3af40c1abcb0c53c09d3c57dbed
Marina <mv.nikolaeva1507@gmail.com>
2022-12-15 17:15:23 +0300

    lesson04

---  lectures/Ex002_Queue.java
+++  lectures/Ex002_Queue.java
@@ -1,5 +1,5 @@
-import java.util.LinkedList;
-import java.util.Queue;
+// import java.util.LinkedList;
+// import java.util.Queue;
 
 // public class Ex002_Queue {
 //     public static void main(String[] args) {
