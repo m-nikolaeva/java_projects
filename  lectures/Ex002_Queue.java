e9cd7eac2638d4b4edeca177a95455fd36258564
Marina <mv.nikolaeva1507@gmail.com>
2022-12-15 17:15:05 +0300

    lesson04

---  lectures/Ex002_Queue.java
+++  lectures/Ex002_Queue.java
@@ -22,16 +22,16 @@ import java.util.Queue;
 
 // }
 
-public class Ex002_Queue {
-public static void main(String[] args) {
-Queue<Integer> queue = new LinkedList<Integer>();
-queue.add(1);
-int item = queue.remove();
-queue.offer(2809);
-item = queue.poll();
-System.out.println(queue);
-queue.remove(2809); // зачем очередь??
-queue.element();
-queue.peek();
-}
-}
+// public class Ex002_Queue {
+// public static void main(String[] args) {
+// Queue<Integer> queue = new LinkedList<Integer>();
+// queue.add(1);
+// int item = queue.remove();
+// queue.offer(2809);
+// item = queue.poll();
+// System.out.println(queue);
+// queue.remove(2809); // зачем очередь??
+// queue.element();
+// queue.peek();
+// }
+// }
