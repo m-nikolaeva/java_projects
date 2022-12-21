470240864d2193765e46a3cf751bfe108b66d122
Marina <mv.nikolaeva1507@gmail.com>
2022-12-21 18:33:15 +0300

    sem2Exception

---
+++ sem2excep1.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class sem2excep1 {
+    public static void main(String[] args) {
+        String pathFile = "test.txt";
+        Map<String, String> map = new HashMap<>();
+        try {
+            List<String> input = Files.readAllLines(Path.of(pathFile));
+            for (String s : input) {
+                String[] line = s.split("=");
+                map.put(line[0], line[1]);
+            }
+            map.forEach((key, value) -> System.out.println("[Key] : " + key + " [Value] : " + value));
+        } catch (IOException exception) {
+            System.err.println(exception.getMessage());
+            System.err.println("error");
+        }
+    }
+}
